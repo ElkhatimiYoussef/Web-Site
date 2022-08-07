7555601231defb01f32dbeae04bf4fdc36e00f3b
ElkhatimiYoussef <110640905+ElkhatimiYoussef@users.noreply.github.com>
2022-08-07 21:57:42 +0100

    craeted pages and configured navigation links

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 def home(request):
   return render(request, 'pages/home.html')#function qui retourn tous qui est a home pages
 
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
